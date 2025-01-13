85174e7d30096e13b19477f47f116b5c6c7804e7
Volkan Sarıkaya <83707556+volkansarikaya71@users.noreply.github.com>
2025-01-13 23:48:55 +0300

    add
    add

---
+++ C#Sharp301/C#Sharp301.EFProject/FrmStatistics.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace C_Sharp301.EFProject
+{
+    public partial class FrmStatistics : Form
+    {
+        public FrmStatistics()
+        {
+            InitializeComponent();
+        }
+
+        EFTravelDbEntities db=new EFTravelDbEntities(); 
+
+        private void FrmStatistics_Load(object sender, EventArgs e)
+        {
+
+            lblLocationCount.Text = db.Location.Count().ToString();
+
+            lblSumCapacity.Text = db.Location.Sum(x=>x.Capacity).ToString();
+
+            lblGuideCount.Text = db.Guide.Count().ToString();
+
+            lblAvgCapacity.Text=db.Location.Average(x=>x.Capacity)?.ToString("F2");
+
+            lblAvgLocationPrice.Text=db.Location.Average(x=>x.Price)?.ToString("F2") + " ₺";
+
+            int LastCountryId = db.Location.Max(x => x.LocationId);
+            lblLastCountryName.Text = db.Location.Where(x => x.LocationId == LastCountryId).Select(y => y.Country).FirstOrDefault();
+
+            lblCappadociaLocationCapacity.Text= db.Location.Where(x => x.City == "Kapadokya" ).Select(y => y.Capacity).FirstOrDefault().ToString();
+
+            lblTurkiyeCapaccityAvg.Text=db.Location.Where(x=>x.Country=="Türkiye").Average(y=>y.Capacity).ToString();
+        
+            var romeguideId=db.Location.Where(x=>x.City== "Roma Turistlik").Select(y=>y.GuideId).FirstOrDefault();
+
+            lblRomeGuideName.Text = db.Guide.Where(x => x.GuideId == romeguideId).Select(y => y.GuideName + " " + y.GuideSurname).FirstOrDefault().ToString();
+        
+            var MaxCapacity=db.Location.Max(x=>x.Capacity);
+            lblMaxCapacityLocation.Text = db.Location.Where(x=>x.Capacity==MaxCapacity).Select(y=>y.City).FirstOrDefault().ToString();
+
+            var MaxPrice = db.Location.Max(x => x.Price);
+            lblMaxPriceLocation.Text = db.Location.Where(x => x.Price == MaxPrice).Select(y => y.City).FirstOrDefault().ToString();
+
+            var guideIdByAysegulCinar = db.Guide.Where(x => x.GuideName== "Ayşegül" && x.GuideSurname== "Çınar").Select(y=>y.GuideId).FirstOrDefault();
+            lblAysegulCinarLocationCount.Text = db.Location.Where(x => x.GuideId == guideIdByAysegulCinar).Count().ToString();
+
+        }
+    }
+}
---
+++ C#Sharp301/C#Sharp301.PresentationLayer/FrmProduct.cs
@@ -0,0 +1,88 @@
+﻿using C_Sharp301.BusinessLayer.Abstract;
+using C_Sharp301.BusinessLayer.Concrete;
+using C_Sharp301.DataAccessLayer.EntityFramework;
+using C_Sharp301.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace C_Sharp301.PresentationLayer
+{
+    public partial class FrmProduct : Form
+    {
+        private readonly IProductService _productService;
+        private readonly ICategoryService _categoryService;
+
+        public FrmProduct()
+        {
+            _productService = new ProductManager(new EfProductDal());
+            _categoryService = new CategoryManager(new EfCategoryDal());
+            InitializeComponent();
+        }
+
+        private void btnList_Click(object sender, EventArgs e)
+        {
+            var productValues = _productService.TGetAll();
+            dataGridView1.DataSource = productValues;
+        }
+
+        private void btnList2_Click(object sender, EventArgs e)
+        {
+            var productValues = _productService.TGetProductsWithCategory();
+            dataGridView1.DataSource = productValues;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            Product product = new Product();
+            product.CategoryId = int.Parse(cmbCategory.SelectedValue.ToString());
+            product.ProductName= txtProductName.Text;
+            product.ProductPrice=decimal.Parse(txtProductPrice.Text);
+            product.ProductStock = int.Parse(txtProductStock.Text);
+            product.ProductDescription=txtDescription.Text;
+            _productService.TInsert(product);
+            MessageBox.Show("Ekleme İşlemi Yapıldı.");
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(txtProductId.Text);
+            var values = _productService.TGetById(id);
+            _productService.TDelete(values);
+            MessageBox.Show("Silme İşlemi Başarıyla Gerçekleşti.");
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(txtProductId.Text);
+            var values= _productService.TGetById(id);
+            values.ProductName = txtProductName.Text;
+            values.ProductPrice=decimal.Parse(txtProductPrice.Text);
+            values.ProductStock= int.Parse(txtProductStock.Text);
+            values.ProductDescription=txtDescription.Text;
+            _productService.TUpdate(values);
+            MessageBox.Show("Güncelleme İşlemi Başarıyla Gerçekleşti.");
+        }
+
+        private void btnGetById_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(txtProductId.Text);
+            var value=_productService.TGetById(id);
+            dataGridView1 .DataSource = value;  
+        }
+
+        private void FrmProduct_Load(object sender, EventArgs e)
+        {
+            var values=_categoryService.TGetAll();
+            cmbCategory.DataSource = values;
+            cmbCategory.DisplayMember = "CategoryName";
+            cmbCategory.ValueMember = "CategoryId";
+        }
+    }
+}
